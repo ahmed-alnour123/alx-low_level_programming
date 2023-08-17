d9b849f9f2c876bf0fc32989c29605bfee5eb8e0
ahmed-alnour123 <ahmed2699@gmail.com>
2023-08-17 17:27:59 +0200

    more functions try 2

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,1 +1,23 @@
-#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+* main - main
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+	unsigned long n = 612852475143;
+	int root = sqrt(n);
+
+	for (i = root; i > 3; i++)
+	{
+		if (n % i == 0)
+		{
+			printf("%d\n", i);
+			return (0);
+		}
+	}
+	return (0);
+}
