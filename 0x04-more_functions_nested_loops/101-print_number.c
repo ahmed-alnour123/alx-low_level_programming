8a21c1e4d0b1743a69b25faef318090eef41ab97
ahmed-alnour123 <ahmed2699@gmail.com>
2023-08-17 17:35:50 +0200

    more functions try 3

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,1 +1,33 @@
 #include "main.h"
+
+/**
+ * print_int - print_int
+ * @n: input
+ */
+void print_int(int n)
+{
+	int i;
+
+	for (i = 1000000000; i >= 1; i /= 10)
+		if (n / i != 0)
+		{
+			_putchar((n / i) % 10 + '0');
+		}
+}
+
+/**
+ * print_number - print_number
+ * @n: input
+ */
+void print_number(int n)
+{
+	if (n == 0)
+		_putchar('0');
+	else if (n < 0)
+	{
+		_putchar('-');
+		print_int(n * -1);
+	}
+	else
+		print_int(n);
+}
