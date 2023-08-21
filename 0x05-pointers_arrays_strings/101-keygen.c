a1e454eef5db5396d9a9237cad6e69b63a7c0cbb
ahmed-alnour123 <ahmed2699@gmail.com>
2023-08-21 14:16:59 +0200

    pointers arrays string try 8

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* get_rand_char - get random character
+* Return: character
+*/
+char get_rand_char(void)
+{
+	return ((rand() % 26) + 'a');
+}
+
+/**
+* main - main
+* Return: 0
+*/
+int main(void)
+{
+	int i, sum, n;
+	char c;
+	char word[100];
+
+	srand(time(NULL));
+	i = 0;
+	sum = 0;
+	n = 2772;
+
+	while (n > 0)
+	{
+		c = get_rand_char();
+		if (n - c > 'z')
+		{
+			n -= c;
+			sum += c;
+			word[i++] = c;
+		}
+		else
+		{
+			n -= c;
+			sum += c;
+			word[i++] = c;
+
+			sum += n;
+			word[i++] = n;
+			break;
+		}
+	}
+
+	printf("%s\n", word);
+	return (0);
+}
