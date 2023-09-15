4ff08ad77bf106bdf07ee6eb3da986f3c080515e
Ahmed Alnour <ahmed2699@gmail.com>
2023-09-15 17:01:10 +0200

    variadic 1

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+/**
+* sum_them_all - sum_them_all
+* @n: input
+* Return: sum
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+	va_end(ap);
+	return (sum);
+}
