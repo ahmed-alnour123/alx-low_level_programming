1ec2aaa4b69b745a97659be57d5f47123a6b0d6f
Ahmed Alnour <ahmed2699@gmail.com>
2023-09-21 13:03:26 +0200

    structs 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,68 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* _strcpy - copies string src to dest
+* @dest: destination string
+* @src: source string
+* Return: pointer to dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (*src != '\0')
+	{
+		dest[i++] = *src++;
+	}
+	dest[i++] = *src++;
+	return (dest);
+}
+
+/**
+* _strlen_recursion - _strlen_recursion
+* @s: input
+* Return: length
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
+
+/**
+* new_dog - new_dog
+* @name: input
+* @age: input
+* @owner: input
+* Return: new dog
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+  int len_name, len_owner;
+  char *name_str, *owner_str;
+  dog_t *dog;
+
+  len_name = _strlen_recursion(name);
+  len_owner = _strlen_recursion(owner);
+
+  name_str = malloc(sizeof(char) * (len_name + 1));
+  owner_str = malloc(sizeof(char) * (len_owner + 1));
+  dog = malloc(sizeof(dog_t));
+
+  if (dog == 0 || name_str == 0 || owner_str == 0)
+    return (0);
+  
+  _strcpy(name_str, name);
+  _strcpy(owner_str, owner);
+
+  dog -> name = name;
+  dog -> age = age;
+  dog -> owner = owner;
+
+  return dog;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,13 +6,16 @@
  * @age: parameter
  * @owner: parameter
  */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
+struct dog {
+  char *name;
+  float age;
+  char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen_recursion(char *s);
+char *_strcpy(char *dest, char *src);
+void free_dog(dog_t *d);
 #endif
